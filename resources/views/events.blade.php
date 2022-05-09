e394aa12007efea0248faddbc3a149fe09496593
yoseplinh26 <90187155+yoseplinh26@users.noreply.github.com>
2022-05-09 22:30:41 +0700

    Update events.blade.php

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -23,7 +23,7 @@
     <link href="{{  asset('css/carousel.css') }}" rel="stylesheet">
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="{{  asset('css/index.css') }}" rel="stylesheet">
+    <link href="{{  asset('css/style.css') }}" rel="stylesheet">
 
     {{-- nav-login --}}
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" /> --}}
